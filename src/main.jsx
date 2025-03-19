7a7fdf26096a5569684229caeb3b2b6831a5f1f5
J27mendez <mendes_j27@hotmail.com>
2025-03-19 15:16:51 -0300

    chore: setup react router with initial pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,23 @@
-import './index.css'
+import "./index.css"
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { BrowserRouter, Route, Routes } from "react-router"
 
-import App from './App.jsx'
+import HomePage from "./pages/home"
+import LoginPage from "./pages/login"
+import NotFoundPage from "./pages/notFound"
+import SignUpPage from "./pages/signUp"
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/#" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
---
+++ src/pages/signUp.jsx
@@ -0,0 +1,5 @@
+const SignUpPage = () => {
+  return <p>Sign Up</p>
+}
+
+export default SignUpPage
