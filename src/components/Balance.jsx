8f9d0e8dc43994d46f9b4b4b7f488b25c0548ba6
J27mendez <mendes_j27@hotmail.com>
2025-04-11 12:49:51 -0300

    feat:  ensures that the amount in the balance does not come with NAN

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -7,6 +7,7 @@ import {
 import { useSearchParams } from "react-router"
 
 import { useGetUserBalance } from "@/api/hooks/user"
+import { safeAmount } from "@/lib/utils"
 
 import BalanceItem from "./BalanceItem"
 
@@ -15,27 +16,28 @@ const Balance = () => {
   const from = searchParams.get("from")
   const to = searchParams.get("to")
   const { data } = useGetUserBalance({ from, to })
+
   return (
     <div className="grid grid-cols-2 grid-rows-2 gap-6">
       <BalanceItem
         label={"Saldo"}
         icon={<WalletIcon size={16} />}
-        amount={data?.balance}
+        amount={safeAmount(data?.balance)}
       />
       <BalanceItem
         label={"Ganhos"}
         icon={<TrendingUpIcon className="text-primary-green" size={16} />}
-        amount={data?.earnings}
+        amount={safeAmount(data?.earnings)}
       />
       <BalanceItem
         label={"Gastos"}
         icon={<TrendingDownIcon className="text-primary-red" size={16} />}
-        amount={data?.expenses}
+        amount={safeAmount(data?.expenses)}
       />
       <BalanceItem
         label={"Investimentos"}
         icon={<PiggyBankIcon className="text-primary-blue" size={16} />}
-        amount={data?.investments}
+        amount={safeAmount(data?.investments)}
       />
     </div>
   )
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
+
+export const safeAmount = (amount) => {
+  return isNaN(amount) || amount === undefined ? 0 : amount
+}
